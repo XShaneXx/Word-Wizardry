b6ec8798cf6f5a99c516b884701d5f9c985b3af7
XShaneXx <xuxufeng294@gmail.com>
2023-11-13 21:59:03 -0500

    100 words & strike & long word more energy

--- Assets/Scripts/CityHealth.cs
+++ Assets/Scripts/CityHealth.cs
@@ -30,6 +30,6 @@ public class CityHealth : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(collision.gameObject);
-        health -= 50f;
+        health -= 10f;
     }
 }
--- Assets/Scripts/TypingManger.cs
+++ Assets/Scripts/TypingManger.cs
@@ -9,17 +9,45 @@ public class TypingManger : MonoBehaviour
     [Header("Word Display")]
     public TMP_Text wordDisplay;
     public TMP_Text nextWordDisplay;
-    private string[] words = { "apple", "banana", "orange", "grape", "cherry" };
+    private string[] words = {
+    "apple", "banana", "orange", "grape", "cherry",
+    "strawberry", "kiwi", "blueberry", "watermelon", "pineapple",
+    "elephant", "giraffe", "lion", "tiger", "zebra",
+    "rhinoceros", "crocodile", "hippopotamus", "kangaroo", "platypus",
+    "computer", "keyboard", "mouse", "monitor", "laptop",
+    "desktop", "software", "hardware", "internet", "programming",
+    "database", "algorithm", "function", "variable", "constant",
+    "condition", "loop", "array", "string", "integer",
+    "floating", "double", "boolean", "char", "spaceship",
+    "alien", "galaxy", "astronaut", "universe", "planet",
+    "star", "constellation", "comet", "asteroid", "telescope",
+    "microscope", "laboratory", "experiment", "chemical", "molecule",
+    "atom", "electron", "proton", "neutron", "energy",
+    "power", "force", "motion", "velocity", "acceleration",
+    "friction", "gravity", "weight", "mass", "density",
+    "volume", "temperature", "pressure", "speed", "distance",
+    "time", "clock", "calendar", "year", "month",
+    "week", "day", "hour", "minute", "second",
+    "century", "decade", "millennium", "past", "present",
+    "future", "history", "geography", "mathematics", "science"
+    };
     private string currentWord;
     private string nextWord;
 
     [Header("Player")]
     public TMP_InputField playerInput;
-    public int score = 0;
     public TMP_Text scoreDisplay;
     public float energy = 10f;
     public float maxEnergy = 100f;
     public Slider energySlider;
+
+    [Header("Points")]
+    public int score = 0;
+    public int consecutiveStrikes = 0;
+    public float timer = 0f;
+    public float strikeTimeThreshold = 5f;
+    public Slider strikesSlider;
+    public TMP_Text strikeDisplay;
 
     void Start()
     {
@@ -34,10 +62,26 @@ public class TypingManger : MonoBehaviour
 
     void Update() 
     {
+        // score display
         scoreDisplay.text = score.ToString();
 
+        // energy slider
         energySlider.value = energy / maxEnergy;
 
+        // strike timer
+        timer += Time.deltaTime;
+
+        if (timer >= strikeTimeThreshold)
+        {
+            consecutiveStrikes = 0;
+            timer = 0f;
+        }
+
+        // strike slider & display
+        strikesSlider.value = 5f - timer;
+        strikeDisplay.text = "+" + consecutiveStrikes.ToString();
+
+        // consume energy
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (energy >= 10)
@@ -77,6 +121,12 @@ public class TypingManger : MonoBehaviour
         {
             score += 100;
 
+            if (timer <= strikeTimeThreshold)
+            {
+                consecutiveStrikes++;
+                timer = 0f;
+            }
+
             // updated new random word
             currentWord = nextWord;
             nextWord = GetRandomWord();
@@ -90,7 +140,17 @@ public class TypingManger : MonoBehaviour
             playerInput.ActivateInputField();
 
             // energy
-            energy += 10;
+            if (consecutiveStrikes > 1)
+            {
+                int wordLength = currentWord.Length;
+                int strikeBouns = wordLength + consecutiveStrikes - 1;
+                energy += strikeBouns;
+            }
+            else
+            {
+                int wordLength = currentWord.Length;
+                energy += wordLength;
+            }
 
             if (energy > maxEnergy)
             {
