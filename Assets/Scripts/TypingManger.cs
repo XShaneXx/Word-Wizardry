779a6e9746f1a2b49f53126edc1e0e428bb29eb5
XShaneXx <xuxufeng294@gmail.com>
2023-11-20 21:47:09 -0500

    Gameover Scene Score Display

--- Assets/Scripts/TypingManger.cs
+++ Assets/Scripts/TypingManger.cs
@@ -42,7 +42,7 @@ public class TypingManger : MonoBehaviour
     public Slider energySlider;
 
     [Header("Points")]
-    public int score = 0;
+    public static int score = 0;
     public int consecutiveStrikes = 0;
     public float timer = 0f;
     public float strikeTimeThreshold = 5f;
